c613fa94861eb0a03d8f38df163d9cd1816e2833
James Gill <james.gill@elev.ga.ntig.se>
2024-04-29 11:57:56 +0200

    first commit

---
+++ Kompletteringmetoder/Program.cs
@@ -0,0 +1,8 @@
+﻿// kompletering metoder i klasser
+
+klass grej = new klass(); // kör en ny instans av klassen klass med namnet grej
+
+string Bokstav = ""; //gör en tom string som heter bokstav
+
+grej.Metod(Bokstav); // kallar metoden för att ge den ett värde
+Console.ReadLine();
---
+++ Kompletteringmetoder/klass.cs
@@ -0,0 +1,12 @@
+
+class klass
+{
+
+    public void Metod(string Bokstav)
+    {
+        Bokstav = "Wow vilken cool metod";
+
+        Console.WriteLine(Bokstav);
+    }
+
+}
